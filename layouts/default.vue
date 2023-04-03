11b1a057e693a15378ad381248b34aefcc9bbce4
Gunjan Patel <gunjan@hobbii.dk>
2023-04-03 10:33:27 +0200

    Fix shadow

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,16 @@
 <template>
-    <main class="mb-4 h-full flex flex-col min-h-screen" :id="page ? 'page-' + page.layout : ''">
+    <main class="mb-4 h-full flex flex-col min-h-screen" :id="page ? 'page-' + page._file.replace('.', '-') : ''">
       <Navbar/>
       <div class="flex-1 overflow-y-auto mx-auto gap-x-8 gap-y-16 lg:max-w-7xl px-4 sm:px-6 lg:px-8 py-20 sm:py-20 xl:col-span-3 2xl:col-span-3">
           <TheTitle v-if="page && page.show_heading">{{ page.title }}</TheTitle>
           <slot />
       </div>
-      <div class="shadow-lg shadow-slate-800 mb-4">&nbsp;</div>
+      <div v-if="path !== '/'" class="shadow-lg shadow-slate-800 mb-4">&nbsp;</div>
       <Footer />
     </main>
 </template>
 <script setup>
 const { page } = useContent()
+const { path } = useRoute()
 </script>
 
